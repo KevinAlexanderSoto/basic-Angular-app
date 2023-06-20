429f838cbf3454b2c8f5e8fec887d2c5c31c8ba3
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-06-20 18:22:10 -0500

    feature: add module for dbz feature

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainPageComponentComponent } from './pages/main-page.component.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainPageComponentComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DbzModule { }
