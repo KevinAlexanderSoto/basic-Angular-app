2612acdffb766431789969822b097617f8a89539
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-07-04 11:39:30 -0500

    add components for character list and form

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component.component';
+import { CharactersListComponent } from './components/characters-list/characters-list.component';
+import { CharacterFormComponent } from './components/character-form/character-form.component';
 
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    CharactersListComponent,
+    CharacterFormComponent
   ],
   exports: [
     MainPageComponent
